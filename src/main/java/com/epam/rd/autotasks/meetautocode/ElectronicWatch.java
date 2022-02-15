5b33c3f56b8fae88f102eafe0d2ef6c3bb79e85b
yuliakordunova97 <kordunovayulia@gmail.com>
2022-02-15 17:05:07 +0200

    done

---
+++ src/main/java/com/epam/rd/autotasks/meetautocode/ElectronicWatch.java
@@ -0,0 +1,13 @@
+package com.epam.rd.autotasks.meetautocode;
+
+import java.util.Scanner;
+
+public class ElectronicWatch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int seconds = scanner.nextInt();
+
+
+
+    }
+}
